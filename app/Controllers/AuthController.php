944fb6fe6e2925d13830e123b1548288f0328a2e
Rachmad Nur Hayat <44340813+rachyharkov@users.noreply.github.com>
2023-10-22 16:48:05 +0700

    init

---
+++ app/Controllers/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class AuthController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
---
+++ app/Controllers/Visitor/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers\Visitor;
+
+use App\Controllers\BaseController;
+
+class HomeController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
